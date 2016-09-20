ce4adf6cf191c5726bb904a6c3a04c8aab61467d
Jason Hanna <jason.hanna01@gmail.com>
2016-09-20 10:16:15 -0600

    add destroy and create to items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -13,4 +13,25 @@ class Api::V1::ItemsController < ApplicationController
     respond_with @item
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      render json: {}
+    end
+  end
+
+  def create
+
+    item = Item.new(item_params)
+    if item.save
+      render json: {}, status:created
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
+
 end
