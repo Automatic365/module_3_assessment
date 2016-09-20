9e7175e94ff6e37e7434c864d7938f6fed22466f
Jason Hanna <jason.hanna01@gmail.com>
2016-09-20 10:47:09 -0600

    working through store services for search by zip

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,7 @@
+class SearchesController < ApplicationController
+
+  def show
+    @stores, @total = Store.find_by_zip(params[:zip])
+  end
+
+end
--- spec/features/user_can_search_store_by_zipcode_spec.rb
+++ spec/features/user_can_search_store_by_zipcode_spec.rb
@@ -1,11 +1,25 @@
-# As a user
-# When I visit "/"
-visit 'root_path'
-# And I fill in a search box with "80202" and click "search"
-fill_in :zipcode, with: "80202"
-click_on "Search for Stores"
-# Then my current path should be "/search" (ignoring params)
-# And I should see stores within 25 miles of 80202
-# And I should see a message that says "17 Total Stores"
-# And I should see exactly 15 results
-# And I should see the long name, city, distance, phone number and store type for each of the 15 results
+require 'rails_helper'
+
+feature 'User can search by zip code' do
+  scenario 'they see a list of stores close to zip code' do
+    VCR.use_cassette("stores") do
+      # As a user
+      # When I visit "/"
+      visit 'root_path'
+      # And I fill in a search box with "80202" and click "search"
+      fill_in :zipcode, with: "80202"
+      click_on "Search for Stores"
+
+      expect(page).to have_content('Search Results')
+      # Then my current path should be "/search" (ignoring params)
+      # And I should see stores within 25 miles of 80202
+      # And I should see a message that says "17 Total Stores"
+      expect(page).to have_content('17 Total Stores')
+      # And I should see exactly 15 results
+      # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+      expect(page).to have_content('Name')
+      expect(page).to have_content('City')
+
+    end
+  end
+end
